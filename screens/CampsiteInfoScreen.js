2c12e5163c92d6277913a3434fccca4d0a8b7c50
mahamed81 <mahamed10ahmed@gmail.com>
2022-08-26 23:24:43 -0500

    added few components that display the campsites in the screen page

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,10 @@
+import RenderCampsite from '../features/campsites/RenderCampsite';
+
+
+const CampsiteInfoScreen = ({ route }) => {
+
+  const { campsite } = route.params;
+  return <RenderCampsite campsite={campsite} />;
+}
+
+export default CampsiteInfoScreen;
